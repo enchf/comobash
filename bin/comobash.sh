1d29c92aeb9c5991c092b968985eabb03410d17e
Ernesto Espinosa <ernesto@helix.re>
2021-02-10 01:32:04 -0600

    Create aliases & functions script

--- bin/comobash.sh
+++ bin/comobash.sh
@@ -1,3 +1,42 @@
 #!/bin/bash
 
-echo "Test"
+# Git aliases
+alias glog="git log --all --graph --decorate --name-only"
+alias gs="git status"
+alias branchlog="git log --graph --abbrev-commit --name-only --decorate --first-parent"
+alias gitscard="git clean -f -d && git reset --hard"
+
+function giff() {
+  git diff **/*.$1
+}
+
+function undogit() {
+  git reset HEAD ./**/*.$1
+  git reset HEAD *.$1
+  git checkout ./**/*.$1
+  git checkout *.$1
+}
+
+function gitadd() {
+  git add **/*.$1
+  git add *.$1
+
+  if [ $# -eq 2 ]
+  then
+    git reset HEAD $2
+  fi
+
+  git status
+}
+
+# Utils for OS
+function portpid() {
+  netstat -vanp tcp | grep $1 | awk '{ print $9 }'
+}
+
+alias reloadsh='source $COMOBASH_TARGET'
+alias showsh='cat $COMOBASH_TARGET'
+alias editsh='nano $COMOBASH_SCRIPT'
+
+# Programming languages
+alias gr='sh gradlew'
--- install.sh
+++ install.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 if [[ $# == 0 ]]; then TARGET="$HOME/.bash_profile"; else TARGET=$1; fi
-CMD="source $(pwd)/bin/comobash.sh"
+SCRIPT="$(pwd)/bin/comobash.sh"
+CMD="source $SCRIPT"
 GREP=$(fgrep "$CMD" $TARGET -x -n)
 
 if [[ -z $GREP ]]; then
     echo "# Comobash command line utils" >> $TARGET
+    echo "export COMOBASH_TARGET=$TARGET" >> $TARGET
+    echo "export COMOBASH_SCRIPT"=$SCRIPT" >> $TARGET
     echo $CMD >> $TARGET
     echo "Installed in file $TARGET"
+    source $TARGET
 else 
     echo "Already installed in file $TARGET"
 fi
